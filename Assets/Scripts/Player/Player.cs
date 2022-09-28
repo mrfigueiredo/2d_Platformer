782b350c12a58dc734f7ad7349aa0dbb3a3dc7ed
mrfigueiredo <14317408+mrfigueiredo@users.noreply.github.com>
2022-09-28 20:27:28 -0300

    Squashed commit of the following:
    commit 58eb2d3c53c7ba58ac79699048af901beec6e684
    Author: mrfigueiredo <14317408+mrfigueiredo@users.noreply.github.com>
    Date:   Wed Sep 28 17:10:33 2022 -0300
    
        Added assets v2
    
    commit 237e65f8802cc618ea75ce09fdf3dcd29436fa54
    Author: mrfigueiredo <14317408+mrfigueiredo@users.noreply.github.com>
    Date:   Wed Sep 28 17:04:28 2022 -0300
    
        Animations added
    
        Add animations for player:
        -Idle;
        -Walk;
        -Run;
        -Jump(WIP);

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
@@ -22,7 +23,20 @@ public class Player : MonoBehaviour
     public Vector2 friction = new Vector2(0.1f, 0f);
 
 
+    [Header("Animation")]
+    public Animator animator;
+
+    public string booleanWalk = "boolWalk";
+    public string triggerJump = "triggerJump";
+    public float animationSpeedRun = 2f;
+    public float jumpStretchX = 0.7f;
+    public float jumpStretchY = 1.2f;
+    public float jumpStretchDuration = 0.2f;
+    public Ease jumpEaseAnimation = Ease.OutBack;
+    public float changeSideDuration = 0.2f;
+
     private float _currentSpeed;
+
 
     private void Update()
     {
@@ -32,23 +46,47 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
-        _currentSpeed = Input.GetKey(runKey) ? speedRun : speed;
+        if (Input.GetKey(runKey))
+        {
+            _currentSpeed = speedRun;
+            animator.speed = animationSpeedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+            animator.speed = 1f;
+        }
 
 
-        if(Input.GetKey(leftKey))
+
+        if (Input.GetKey(leftKey))
         {
             rigidBody.velocity = new Vector2(-_currentSpeed, rigidBody.velocity.y);
+            if (rigidBody.transform.localScale.x != -1)
+            {
+                rigidBody.transform.DOScaleX(-1, changeSideDuration);
+            }
+            animator.SetBool(booleanWalk, true);
         }
-        else if(Input.GetKey(rightKey))
+        else if (Input.GetKey(rightKey))
         {
             rigidBody.velocity = new Vector2(_currentSpeed, rigidBody.velocity.y);
+            if (rigidBody.transform.localScale.x != 1)
+            {
+                rigidBody.transform.DOScaleX(1, changeSideDuration);
+            }
+            animator.SetBool(booleanWalk, true);
+        }
+        else
+        {
+            animator.SetBool(booleanWalk, false);
         }
 
-        if(rigidBody.velocity.x > 0)
+        if (rigidBody.velocity.x > 0)
         {
             rigidBody.velocity -= friction;
         }
-        else if(rigidBody.velocity.x < 0)
+        else if (rigidBody.velocity.x < 0)
         {
             rigidBody.velocity += friction;
         }
@@ -56,10 +94,20 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if(Input.GetKeyDown(jumpKey))
+        if (Input.GetKeyDown(jumpKey))
         {
 
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+
+            JumpAnimation();
         }
+    }
+
+    private void JumpAnimation()
+    {
+        animator.SetTrigger(triggerJump);
+        DOTween.Kill(rigidBody.transform);
+        rigidBody.transform.DOScaleY(jumpStretchY, jumpStretchDuration).SetEase(jumpEaseAnimation).SetLoops(2, LoopType.Yoyo);
+        rigidBody.transform.DOScaleX(rigidBody.transform.localScale.x * jumpStretchX, jumpStretchDuration).SetEase(jumpEaseAnimation).SetLoops(2, LoopType.Yoyo);
     }
 }
